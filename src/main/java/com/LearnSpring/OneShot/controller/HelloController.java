fde1011611f907b5ad077ef5917be2533c4089eb
va-run-6626 <vbhutada143@gmail.com>
2024-09-09 11:04:26 +0530

    First API

---
+++ src/main/java/com/LearnSpring/OneShot/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.LearnSpring.OneShot.controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String HelloWorld(){
+        return "First API";
+    }
+}
