f05b14393218883d2a0562231f30ddcc03d1afae
va-run-6626 <vbhutada143@gmail.com>
2024-09-09 10:13:47 +0530

    Added readme and GIT

---
+++ src/main/java/com/LearnSpring/OneShot/OneShotApplication.java
@@ -0,0 +1,13 @@
+package com.LearnSpring.OneShot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneShotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneShotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/LearnSpring/OneShot/OneShotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LearnSpring.OneShot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneShotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
